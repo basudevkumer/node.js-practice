52b04748a49a9bb7fdc785d29f24910e07b410d9
basudevkumer <codebyjhulon.bd@gmail.com>
2026-06-16 00:32:03 +0600

    practice writeHead

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const http =  require("http")
+const http = require("http");
 
-const server =  http.createServer((req,res)=>{
-    
-})
+const server = http.createServer((req, res) => {
+  //   console.log(req.url);
+  //   console.log(req.method);
+
+  res.writeHead(200, { // 200-success, 201-new-resorce create, 404-page not found , 500- server error
+    "Content-Type": "text/html",
+  });
+
+  res.end("<h1>Hello</h1>");
+});
+
+server.listen(5000);
