42296f2b931f3a18178e321390c7bee8dcd20c4f
basudevkumer <codebyjhulon.bd@gmail.com>
2026-06-16 01:21:54 +0600

    http practice

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
- if(req.url === "/"){
-    res.end("Home")
- }else if(req.url === "/about"){
+  if (req.url === "/" && req.method === "GET") {
+    res.end("Home");
+  }else if(req.url === "/about" && req.method === "GET"){
     res.end("About")
- }
+  }else{
+    res.writeHead(404);
+    res.end("404 not found!")
+  }
 });
 
-server.listen(5000);
+
+server.listen(5000, ()=>{
+    console.log("server is running...");
+    
+})
