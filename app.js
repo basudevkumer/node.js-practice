b9b81d05705e37b0ef3dc2581a6be57314d0c73e
basudevkumer <codebyjhulon.bd@gmail.com>
2026-06-16 00:35:45 +0600

    learn manually http route

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  //   console.log(req.url);
-  //   console.log(req.method);
-
-  res.writeHead(200, { // 200-success, 201-new-resorce create, 404-page not found , 500- server error
-    "Content-Type": "text/html",
-  });
-
-  res.end("<h1>Hello</h1>");
+ if(req.url === "/"){
+    res.end("Home")
+ }else if(req.url === "/about"){
+    res.end("About")
+ }
 });
 
 server.listen(5000);
